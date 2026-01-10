56d6e7cb5c1b54fe9523b437c5bef4cba5c34839
Gaurav Shakya <gauravshakya@gmail.com>
2026-01-10 19:44:33 +0530

    AK

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
-function add(a,b){
-    return a+b;
-}
-var result = add(2,56)
-console.log(result);
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
